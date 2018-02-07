0f7e94d4b0cb92ba36cdaf9bb947cbe641de80f4
nooraja <N00@liquid.local>
2018-02-08 00:01:30 +0700

    first commit

---
+++ Expense/SplashViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SplashViewController.swift
+//  Expense
+//
+//  Created by Muhammad Noor on 07/02/2018.
+//  Copyright © 2018 Muhammad Noor. All rights reserved.
+//
+
+import UIKit
+
+class SplashViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
